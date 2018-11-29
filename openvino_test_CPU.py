1d6902b4cf9a22cf31a7892fa4a95132dfcc2cde
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-11-30 00:57:55 +0900

    update

--- openvino_test_CPU.py
+++ openvino_test_CPU.py
@@ -22,22 +22,29 @@ elapsedTime = 0
 #plugin.set_initial_affinity(net)
 
 #plugin = IEPlugin(device="MYRIAD")
-
+#plugin = IEPlugin(device="GPU")
 plugin = IEPlugin(device="CPU")
 
 plugin.add_cpu_extension("/home/alpha/inference_engine_samples_build/intel64/Release/lib/libcpu_extension.so")
 exec_net = plugin.load(network=net)
 
-input_blob = next(iter(net.inputs))        #input_blob = 'input'
-out_blob   = next(iter(net.outputs))       #out_blob   = 'output/BiasAdd'
-n, c, h, w = net.inputs[input_blob].shape  #n, c, h, w = 1, 3, 256, 256
+input_blob = next(iter(net.inputs))  #input_blob = 'data'
+out_blob   = next(iter(net.outputs)) #out_blob   = 'argmax'
+print("input_blob =", input_blob)
+print("out_blob =", out_blob)
+n, c, h, w = net.inputs[input_blob].shape  #n, c, h, w = 1, 3, 1024, 2048
 
 del net
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 10)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height)
+#cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FPS, 10)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_width)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_height)
+
+cap = cv2.VideoCapture("data/input/testvideo.mp4")
+camera_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+camera_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 time.sleep(1)
 
 while cap.isOpened():
@@ -56,11 +63,11 @@ while cap.isOpened():
     exec_net.start_async(request_id=0, inputs={input_blob: prepimg})
 
     if exec_net.requests[0].wait(-1) == 0:
-        outputs = exec_net.requests[0].outputs[out_blob] # (1, 3, 2048, 1024)
+        outputs = exec_net.requests[0].outputs[out_blob] # (1, 1, 1024, 2048)
         print("SegmentationTime = {:.7f}".format(time.perf_counter() - t2))
         outputs = outputs[0][0]
         print(outputs.shape)
-        outputs = cv2.resize(outputs, (camera_width, camera_height)) # (320, 240, 3)
+        outputs = cv2.resize(outputs, (camera_width, camera_height))
 
         # View
         image = Image.fromarray(np.uint8(outputs), mode="P")
